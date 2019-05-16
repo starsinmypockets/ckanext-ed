2a037356412bb4a56c8c21ac2b6f3a4c26ae72bb
Tanvir Chahal <tnvr009@gmail.com>
2019-05-16 15:51:55 +0530

    Homepage Styling as per mockups and Bugfixes (#131)
    * Updated navbar and top header
    
    * Fixed the link errors
    
    * Structured the about page
    
    * Added scroll spy
    
    * styled the page sidebar
    
    * about page content and view more dataset section
    
    * fixed header
    
    * Sidebar styled
    
    * sidebar javascripts included
    
    * updated scroll location logic for sidebar
    
    * updated footer for the pages with logo
    
    * setting up hero section on homepage
    
    * updated hero section for responsiveness
    
    * added banner image for hero section
    
    * Setting up dataset categories for homepage
    
    * Added trending and new data sections and design
    
    * fixed homepage anchors
    
    * split up old sidebar from custom sidebar
    
    * custom grid for sidebar
    
    * styled the homepage tags
    
    * categories styling on homepage
    
    * Changed the simplebar js from cdn to local
    
    * introduce particles
    
    * fine tune particles
    
    * stats error removed
    
    * fixed homepage particles bug and responsive issues
    
    * responsive fixes for homepage elements
    
    * fixed font size issues on trending-new homepage
    
    * Homepage trending and new data using dynamic values
    
    * Updated trending and new data organization names to be dynamic
    
    * Bugfix: Updated search and header footer for search bug - fixes #130 
    
    * Added missing span

--- ckanext/ed/fanstatic/js/main.js
+++ ckanext/ed/fanstatic/js/main.js
@@ -2,16 +2,9 @@ $('body').scrollspy({ target: '#spy', offset:280});
 
 
 //Getting sidebar height
-$(document).ready(function(){
-    calcSidebarHeight();
-});
 
-$(window).resize(function(){
-    calcSidebarHeight();
-});
 
 $(window).on('load',function(){
-    calcSidebarHeight();
     calcScrollLocation();
 });
 
@@ -22,7 +15,7 @@ $(window).scroll(function() {
 function calcSidebarHeight(){
     var height = $(".module-content").outerHeight();
     $(".sidebar").height(height);
-}
+};
 
 function calcScrollLocation(){
     var scroll = $(window).scrollTop();
@@ -33,4 +26,102 @@ function calcScrollLocation(){
     } else {
         $('.sidebar_content_wrap').removeClass("fixed");
     }
+}
+
+
+$(document).ready(function(){
+
+    //Checks the page name via page tag
+    var page = $('body').find('.page-home').text();
+
+    /* Particles for homepage */
+    if(page=="home"){
+        renderParticles();
+    }
+    
+}); 
+
+function renderParticles(){
+    particlesJS("hero", {
+        "particles": {
+            "number": {
+                "value":15, "density": {
+                    "enable": true, "value_area": 1000
+                }
+            }
+            , "color": {
+                "value": ["#3da650", "#0372e4"]
+            }
+            , "shape": {
+                "type":"edge", "stroke": {
+                    "width": 0, "color": "#000000"
+                }
+                , "polygon": {
+                    "nb_sides": 10
+                }
+                , "image": {
+                    "src": "img/github.svg", "width": 100, "height": 100
+                }
+            }
+            , "opacity": {
+                "value":1, "random":false, "anim": {
+                    "enable": false, "speed": 1, "opacity_min": 0, "sync": false
+                }
+            }
+            , "size": {
+                "value":5.5, "random":true, "anim": {
+                    "enable": false, "speed": 4, "size_min": 3.5, "sync": false
+                }
+            }
+            , "line_linked": {
+                "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1
+            }
+            , "move": {
+                "enable":true, "speed":1, "direction":"none", "random":true, "straight":false, "out_mode":"out", "bounce":false, "attract": {
+                    "enable": false, "rotateX": 600, "rotateY": 600
+                }
+            }
+        }
+        , "interactivity": {
+            "detect_on":"canvas", "events": {
+                "onhover": {
+                    "enable": false, "mode": "bubble"
+                }
+                , "onclick": {
+                    "enable": false, "mode": "repulse"
+                }
+                , "resize":true
+            }
+            , "modes": {
+                "grab": {
+                    "distance":400, "line_linked": {
+                        "opacity": 1
+                    }
+                }
+                , "bubble": {
+                    "distance": 85.26810729164123, "size": 0, "duration": 2, "opacity": 0, "speed": 3
+                }
+                , "repulse": {
+                    "distance": 400, "duration": 0.4
+                }
+                , "push": {
+                    "particles_nb": 4
+                }
+                , "remove": {
+                    "particles_nb": 2
+                }
+            }
+        }
+        , "retina_detect":true
+    });
+
+
+    function calcCanvasHeight(){
+        setTimeout(function() { 
+            $(".hero > canvas").height("400px");
+            $(".hero > canvas").attr("height",400);
+         },1);
+    }
+    
+
 }
