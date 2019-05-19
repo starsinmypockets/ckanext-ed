0b91c5b98e21b03e369ebbb62ae664e39a8d6158
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-05-19 08:36:42 +0400

    bugfix in alphabetize

--- ckanext/ed/helpers.py
+++ ckanext/ed/helpers.py
@@ -275,5 +275,5 @@ def load_choices(field_meta=None):
 
 
 def alphabetize_dict(items, sort_by='display_name'):
-    sorted_dict = sorted(items, key=lambda x: x.get('sort_by'))
+    sorted_dict = sorted(items, key=lambda x: x.get(sort_by))
     return sorted_dict
